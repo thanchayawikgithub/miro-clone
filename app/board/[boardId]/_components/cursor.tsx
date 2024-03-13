b6c7695fe0c6e119c7ffcc8a3dbfce978f232a21
thanchayawikgithub <thanchayawik@gmail.com>
2024-03-13 20:50:02 +0700

    update canvas cursors

--- app/board/[boardId]/_components/cursor.tsx
+++ app/board/[boardId]/_components/cursor.tsx
@@ -17,8 +17,6 @@ export const Cursor = memo(({ connectionId }: CursorProps) => {
 
   const name = info?.name || "Teammate";
 
-  console.log(info, cursor);
-
   if (!cursor) {
     return null;
   }
@@ -29,7 +27,7 @@ export const Cursor = memo(({ connectionId }: CursorProps) => {
     <foreignObject
       style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
       height={50}
-      width={50}
+      width={name.length * 10 + 24}
       className="relative drop-shadow-md"
     >
       <MousePointer2
@@ -39,6 +37,12 @@ export const Cursor = memo(({ connectionId }: CursorProps) => {
           color: connectionIdToColor(connectionId),
         }}
       />
+      <div
+        className="absolute left-5  px-1.5 py-0.5 rounded-md text-xs text-white font-semibold"
+        style={{ backgroundColor: connectionIdToColor(connectionId) }}
+      >
+        {name}
+      </div>
     </foreignObject>
   );
 });
